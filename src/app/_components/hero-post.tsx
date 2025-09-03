414f080e7889b899d94eb8431068950b91a897c5
Eva Machova <evca.machova@gmail.com>
2025-09-03 21:49:25 +0200

    Edit favicons

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -12,9 +12,9 @@ type Props = {
 
 export function HeroPost({ title, coverImage, date, excerpt, slug }: Props) {
   return (
-    <section className="w-full bg-accentBg p-6 md:w-4/5 mb-8 md:mb-16">
+    <section className="w-full bg-accentBg p-4 md:p-6 md:pb-10 md:w-4/5 mb-8 md:mb-16">
       <Link className="group" href={`/journal/${slug}`}>
-        <div className="text text-right text-stone-900 mb-4 text-sm md:text-lg group-hover:underline">
+        <div className="text text-right text-stone-900 md:mb-2 text-sm md:text-lg group-hover:underline">
           <DateFormatter dateString={date} />
         </div>
         <div>
